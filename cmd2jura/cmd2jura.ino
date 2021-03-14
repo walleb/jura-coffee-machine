7397aa9f427181f52b8315b567956e38e1bf513f
Björn Wallebäck <bjorn@memaking.se>
2021-03-14 14:04:12 +0100

    Add OTA update capability

--- cmd2jura/cmd2jura.ino
+++ cmd2jura/cmd2jura.ino
@@ -31,13 +31,14 @@
 #include <ESP8266WebServer.h>
 #include <WiFiManager.h>
 #include <SoftwareSerial.h>
+#include <ArduinoOTA.h>
 
 #define WIFINAME  "JURA"
 
-#define GPIORX    12
-#define GPIOTX    13
+#define GPIORX    4
+#define GPIOTX    5
 
-#define GPIOLEDIO     4
+#define GPIOLEDIO     2
 #define GPIOLEDPULSE  0
 
 SoftwareSerial softserial(GPIORX, GPIOTX);
@@ -128,6 +129,9 @@ void handle_web() {
 }
 
 void setup() {
+  Serial.begin(115200);
+  Serial.println("Booting");
+  
   // put your setup code here, to run once:
 
   wifi_station_set_hostname(WIFINAME);
@@ -144,10 +148,44 @@ void setup() {
   pinMode(GPIOLEDIO, OUTPUT);
   pinMode(GPIOLEDPULSE, OUTPUT);
 
+  // Hostname defaults to esp8266-[ChipID]
+  ArduinoOTA.setHostname("JURA ESP");
+
+  ArduinoOTA.onStart([]() {
+    String type;
+    if (ArduinoOTA.getCommand() == U_FLASH)
+      type = "sketch";
+    else // U_SPIFFS
+      type = "filesystem";
+    // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
+    Serial.println("Start updating " + type);
+  });
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nEnd");
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+    else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+    else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+    else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+    else if (error == OTA_END_ERROR) Serial.println("End Failed");
+  });
+
+  ArduinoOTA.begin();
+  Serial.println("Ready");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
 }
 
 int i = 0;
 void loop() {
+  ArduinoOTA.handle();
+  
   // put your main code here, to run repeatedly:
 
   webserver.handleClient();
@@ -155,4 +193,3 @@ void loop() {
   i = (i + 1) % 200000;
   digitalWrite(GPIOLEDPULSE, (i < 100000) ? LOW : HIGH);
 }
-
